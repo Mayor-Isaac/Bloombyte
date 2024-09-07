a449367d7a7be17fa115561fb0e8302af484801c
apple <mac@MACs-MacBook-Pro.local>
2024-09-07 10:06:24 +0100

    update

--- svgs/user.tsx
+++ svgs/user.tsx
@@ -3,13 +3,19 @@ import React from "react";
 const User = () => {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 24 24">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="5em"
+        height="5em"
+        viewBox="0 0 24 24"
+      >
         <path
           fill="none"
-          stroke="rgb(80, 130, 221)"
+          stroke="#0044AD"
           strokeLinecap="round"
           strokeWidth={2}
-          d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0a3 3 0 0 1 6 0Z"></path>
+          d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0a3 3 0 0 1 6 0Z"
+        ></path>
       </svg>
     </>
   );
