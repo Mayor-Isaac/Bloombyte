46472917663c34eeed0b06dc8cdd1238d1e97d11
apple <mac@MACs-MacBook-Pro.local>
2024-09-17 21:21:13 +0100

    update

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,21 +1,40 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import { menuLinks } from "../../utils/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Bars from "@/svgs/bars";
+import Image from "next/image";
 
 const Header = () => {
   const pathname = usePathname();
+  const [mobile, setMobile] = useState(false);
   return (
-    <div className={styles.headContain}>
+    <div
+      className={
+        mobile ? `${styles.mobile} ${styles.headContain}` : styles.headContain
+      }
+    >
       <div className={styles.logo}>
-        <h2>BloomByte</h2>
+        <Image
+          src="/images/bloombyte_horizontal.png"
+          alt="Logo"
+          width={120}
+          height={30}
+        />
+        <div onClick={() => setMobile(!mobile)}>
+          <Bars />
+        </div>
       </div>
       <div className={styles.meunLinks}>
         {menuLinks?.map((item, index) => {
           return (
-            <Link href={item.link} key={index} className={pathname === item.link ? styles.active : ""}>
+            <Link
+              href={item.link}
+              key={index}
+              className={pathname === item.link ? styles.active : ""}
+            >
               {item.title}
             </Link>
           );
